ef35bace9d44e067c2ad82e9e1a59346c3e91593
langzhijie <907239639@qq.com>
2020-02-01 01:12:58 +0800

    测试

--- web_back/js/index.js/article.js
+++ web_back/js/index.js/article.js
@@ -1,8 +1,8 @@
 // 所有关于文章相关的操作
  var article = {
      // 文章类别
-     get:function (page) {
-      return  $.get(PATH.article_get,{"page":page});
+     get:function (page,type,status) {
+      return  $.get(PATH.article_get,{"page":page,"type":type,"state":status});
     
      },
  
